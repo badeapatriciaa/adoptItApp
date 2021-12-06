323543e5b978a62be6a6621cdb60cf9fd93b2c7f
ZZ02SR826 <nicuavram11@gmail.com>
2021-12-06 16:31:41 +0200

    tables for all animals and my animals

--- frontend/src/components/AnimalUserListComponent.vue
+++ frontend/src/components/AnimalUserListComponent.vue
@@ -1,137 +1,59 @@
 <template>
-  <div class="q-pa-md">
-    <q-table
-      style="height: 400px"
-      title="Treats"
-      :rows="rows"
-      :columns="columns"
-      row-key="index"
-      virtual-scroll
-      v-model:pagination="pagination"
-      :rows-per-page-options="[0]"
-    />
-  </div>
+    <div>
+        <DataTable :value="animals" :scrollable="true" scrollHeight="400px">
+            <Column
+                field="animalBreed"
+                header="Breed"
+                style="min-width: 200px"
+            ></Column>
+            <Column
+                field="animalDescription"
+                header="Description"
+                style="min-width: 200px"
+            ></Column>
+            <Column
+                field="animalName"
+                header="Name"
+                style="min-width: 200px"
+            ></Column>
+            <Column
+                field="animalType"
+                header="Type"
+                style="min-width: 200px"
+            ></Column>
+        </DataTable>
+    </div>
 </template>
 
 <script>
-import { ref } from "vue";
-
-const columns = [
-  {
-    name: "index",
-    label: "#",
-    field: "index",
-  },
-
-  {
-    name: "Breed",
-    required: true,
-    label: "Breed",
-    align: "left",
-    field: (row) => row.name,
-    format: (val) => `${val}`,
-    sortable: true,
-  },
-  {
-    name: "Description",
-    required: true,
-    label: "Description",
-    align: "left",
-    field: (row) => row.name,
-    format: (val) => `${val}`,
-    sortable: true,
-  },
-  {
-    name: "Gender",
-    required: true,
-    label: "Gender",
-    align: "left",
-    field: (row) => row.name,
-    format: (val) => `${val}`,
-    sortable: true,
-  },
-  {
-    name: "Name",
-    required: true,
-    label: "Name",
-    align: "left",
-    field: (row) => row.name,
-    format: (val) => `${val}`,
-    sortable: true,
-  },
-  {
-    name: "Type",
-    required: true,
-    label: "Type",
-    align: "left",
-    field: (row) => row.name,
-    format: (val) => `${val}`,
-    sortable: true,
-  },
-
-  // { name: "fat", label: "Fat (g)", field: "fat", sortable: true },
-  // { name: "carbs", label: "Carbs (g)", field: "carbs" },
-  // { name: "protein", label: "Protein (g)", field: "protein" },
-  // { name: "sodium", label: "Sodium (mg)", field: "sodium" },
-  // {
-  //   name: "calcium",
-  //   label: "Calcium (%)",
-  //   field: "calcium",
-  //   sortable: true,
-  //   sort: (a, b) => parseInt(a, 10) - parseInt(b, 10),
-  // },
-  // {
-  //   name: "iron",
-  //   label: "Iron (%)",
-  //   field: "iron",
-  //   sortable: true,
-  //   sort: (a, b) => parseInt(a, 10) - parseInt(b, 10),
-  // },
-];
-
-import axios from "axios";
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
+import axios from 'axios';
 export default {
-  name: "AnimalUserListComponent",
-  data() {
-    return {
-      animals: [],
-      showModal: false,
-      rows: [],
-    };
-  },
-  beforeMount() {
-    try {
-      let userLoggedIn = localStorage.getItem("userId");
-      axios.get("http://localhost:5050/animals/" + userLoggedIn).then((res) => {
-        this.animals = res.data.animals;
-        console.log(JSON.stringify(res.data));
-        console.log("animale sunt " + JSON.stringify(this.animals));
-        for (let i = 0; i < this.animals.length; i++) {
-          this.rows = this.rows.concat(
-            this.animals.map((r, j) => ({
-              ...r,
-              index: i * this.animals.length + j + 1,
-            }))
-          );
-          console.log(this.rows);
+    name: 'AnimalUserListComponent',
+    components: {
+        DataTable,
+        Column,
+    },
+    data() {
+        return {
+            animals: [],
+        };
+    },
+    mounted() {
+        try {
+            let userLoggedIn = localStorage.getItem('userId');
+            axios
+                .get('http://localhost:5050/animals/' + userLoggedIn)
+                .then(res => {
+                    console.log(res);
+                    this.animals = res.data.animals;
+                });
+        } catch (err) {
+            console.log(
+                'Error during retrieving animals from database: ' + err.message
+            );
         }
-      });
-    } catch (err) {
-      console.log(
-        "Error during retrieving animals from database: " + err.message
-      );
-    }
-  },
-  setup() {
-    return {
-      columns,
-
-      //rows,
-
-      pagination: ref({
-        rowsPerPage: 5,
-      }),
-    };
-  },
+    },
 };
 </script>
--- frontend/src/components/AnimalsAllComponent.vue
+++ frontend/src/components/AnimalsAllComponent.vue
@@ -1,85 +1,94 @@
 <template>
-  <table class="table table-striped table-bordered">
-    <thead>
-      <tr>
-        <th>Type</th>
-        <th>Breed</th>
-        <th>Name</th>
-        <th>Gender</th>
-        <th>Description</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr v-for="animal in animals" :key="animal.id">
-        <td>{{ animal.animalType }}</td>
-        <td>{{ animal.animalBreed }}</td>
-        <td>{{ animal.animalName }}</td>
-        <td>{{ animal.animalGender }}</td>
-        <td>{{ animal.animalDescription }}</td>
-      </tr>
-    </tbody>
-  </table>
+    <div>
+        <DataTable :value="animals" :scrollable="true" scrollHeight="600px">
+            <Column
+                field="animalBreed"
+                header="Breed"
+                style="min-width: 200px"
+            ></Column>
+            <Column
+                field="animalDescription"
+                header="Description"
+                style="min-width: 200px"
+            ></Column>
+            <Column
+                field="animalName"
+                header="Name"
+                style="min-width: 200px"
+            ></Column>
+            <Column
+                field="animalType"
+                header="Type"
+                style="min-width: 200px"
+            ></Column>
+        </DataTable>
+    </div>
 </template>
 
 <script>
-import axios from "axios";
+import axios from 'axios';
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
 
 export default {
-  name: "AnimalsAllComponent",
-  data() {
-    return {
-      animals: [],
-      showModal: false,
-    };
-  },
-  beforeMount() {
-    try {
-      axios
-        .get("http://localhost:5050/animals" + "/getAllAnimals")
-        .then((res) => {
-          this.animals = res.data.animals;
-        });
-    } catch (err) {
-      console.log(
-        "Error during retrieving animals from database: " + err.message
-      );
-    }
-  },
+    name: 'AnimalsAllComponent',
+    components: {
+        DataTable,
+        Column,
+    },
+    data() {
+        return {
+            animals: [],
+        };
+    },
+    beforeMount() {
+        try {
+            axios
+                .get('http://localhost:5050/animals' + '/getAllAnimals')
+                .then(res => {
+                    this.animals = res.data.animals;
+                });
+        } catch (err) {
+            console.log(
+                'Error during retrieving animals from database: ' + err.message
+            );
+        }
+    },
 };
 </script>
 
 <style>
 .formContainer {
-  width: 100%;
-  display: flex;
-  justify-content: center;
-  background-color: #64686b;
-  padding: 2%;
-  min-height: 100vh;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    background-color: #64686b;
+    padding: 2%;
+    min-height: 100vh;
 }
 
 .formHeading {
-  font-size: 28px;
-  text-transform: uppercase;
-  font-weight: bold;
-  letter-spacing: 1px;
-  color: #64686b;
+    font-size: 28px;
+    text-transform: uppercase;
+    font-weight: bold;
+    letter-spacing: 1px;
+    color: #64686b;
 }
 .registerBtn {
-  margin-top: 30px;
-  margin-bottom: 30px;
-  background-color: #64686b;
+    margin-top: 30px;
+    margin-bottom: 30px;
+    background-color: #64686b;
 }
 .formCard {
-  width: 50%;
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+    width: 50%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
 }
 
 .formContainer .formCard .formInput {
-  width: 50%;
-  padding: 10px 10px 35px 10px !important;
+    width: 50%;
+    padding: 10px 10px 35px 10px !important;
 }
 </style>
